524b3e68cc0cc76b09aa78d6ff54f176f243d349
Patryk Majchrzak <pmaj12345@gmail.com>
2024-08-02 20:40:08 +0200

    adding margin for Sections

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -5,4 +5,5 @@ export const SectionContainer = styled.section`
     box-shadow:0px 16px 58px 0px rgba(9, 10, 51, 0.03),
     0px -2px 50px 0px rgba(9, 10, 51, 0.02);
     padding:32px;
+    margin:72px auto;
 `
