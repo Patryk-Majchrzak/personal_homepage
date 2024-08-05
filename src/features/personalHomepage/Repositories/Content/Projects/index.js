b3ab7d3e63197c40d3c2d6c44a07eb095fe8d7f8
Patryk Majchrzak <pmaj12345@gmail.com>
2024-08-05 11:56:10 +0200

    managed listing repositories with links

--- src/features/personalHomepage/Repositories/Content/Projects/index.js
+++ src/features/personalHomepage/Repositories/Content/Projects/index.js
@@ -10,8 +10,30 @@ export const Projects = () => {
         <ul>
             {repositories.map(repository => (
                 <li key={repository.name}>
-                    <h2>{repository.name}</h2>
-                    <p>{repository.description}</p>
+                    <div>
+                        <h2>{repository.name}</h2>
+                        <p>{repository.description}</p>
+                        <p>
+                            Demo: <a
+                                href={repository.homepage}
+                                target="_blank"
+                                rel="norefferer noopener"
+                                title="link do strony"
+                            >
+                                {repository.homepage}
+                            </a>
+                        </p>
+                        <p>
+                            Code: <a
+                                href={repository.html_url}
+                                target="_blank"
+                                rel="norefferer noopener"
+                                title="link do projektu na GitHubie"
+                            >
+                                {repository.html_url}
+                            </a>
+                        </p>
+                    </div>
                 </li>
             ))}
         </ul>
