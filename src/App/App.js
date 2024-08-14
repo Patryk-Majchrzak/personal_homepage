7569f13d4a3776a9b74568a950a9721efa9b5356
Patryk Majchrzak <pmaj12345@gmail.com>
2024-08-14 19:38:11 +0200

    putting Footer inside Main

--- src/App/App.js
+++ src/App/App.js
@@ -28,8 +28,8 @@ function App() {
             <Skills />
             <WantToLearn />
             <Repositories />
+            <Footer />
           </Main>
-          <Footer />
         </Container>
       </ThemeProvider>
     </>
