7e24b82e9b724e50f01f06e6560fc9fc4b8e01c2
Patryk Majchrzak <pmaj12345@gmail.com>
2024-08-14 19:38:30 +0200

    adding transition to background color

--- src/App/styled.js
+++ src/App/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     background-color: ${({theme}) => theme.colors.websiteBackground};
+    transition: background 0.2s
 `
 
 export const Main = styled.main`
